2754ad01581efe3995863620bd16be12ef9feaaa
Oguzhan Karaduman <oguzhan.kduman@gmail.com>
2018-02-05 23:18:10 +0300

    LeetCode-LongestSubstringWithoutRepeatingCharacters problem is solved.

--- LeetCode-LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/src/main/java/com/leetcode/longestsubstringwithoutrepeatingcharacters/App.java
+++ LeetCode-LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/src/main/java/com/leetcode/longestsubstringwithoutrepeatingcharacters/App.java
@@ -8,6 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(getLongestSubstringWithoutRepeatingCharacters("abcabcbb"));
+    }
+
+    public static int getLongestSubstringWithoutRepeatingCharacters(String text)
+    {
+        String result = "";
+        for(int i=0; i<text.length(); i++)
+        {
+            CharSequence cs = ""+text.charAt(i);
+            if(!result.contains(cs))
+            {
+                result+=(cs);
+            }
+        }
+        if(result.length()>0)
+            return result.length();
+        else
+            return 0;
     }
 }
